596933a0f8f7a9584b8a51343ad40935e15bec96
zq22843 <zq22843@bristol.ac.uk>
2023-10-31 13:25:43 -0400

    now have a working springboot to start creating the layout of the website

---
+++ OnlineDeliveryPlatform(1)/OnlineDeliveryPlatform/src/test/java/com/example/OnlineDeliveryPlatform/OnlineDeliveryPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OnlineDeliveryPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineDeliveryPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
