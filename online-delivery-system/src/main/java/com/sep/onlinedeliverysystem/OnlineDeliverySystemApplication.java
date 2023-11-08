ab2f9ecd4fa1616136ddfd5bb235c7c7422d3c15
jp-ruiz <vt22223@bristol.ac.uk>
2023-11-08 11:31:58 +0000

    New app setup and updated gitignore

---
+++ online-delivery-system/src/main/java/com/sep/onlinedeliverysystem/OnlineDeliverySystemApplication.java
@@ -0,0 +1,13 @@
+package com.sep.onlinedeliverysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineDeliverySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineDeliverySystemApplication.class, args);
+	}
+
+}
---
+++ online-delivery-system/src/main/java/com/sep/onlinedeliverysystem/controller/WebController.java
@@ -0,0 +1,35 @@
+package com.sep.onlinedeliverysystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class WebController {
+    @GetMapping("/home")
+    public String home(){ return "home"; }
+
+    @GetMapping("/login")
+    @ResponseBody
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/order")
+    @ResponseBody
+    public String order(){
+        return "order";
+    }
+
+    @GetMapping("/partner")
+    @ResponseBody
+    public String partner(){
+        return "partner";
+    }
+
+    @GetMapping("/signup")
+    @ResponseBody
+    public String signup(){
+        return "signup";
+    }
+}
