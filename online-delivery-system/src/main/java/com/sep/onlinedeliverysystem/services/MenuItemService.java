1a8a17b1b26ce28ef12dc803a57ddbd09923dd99
Hugh <bz22499@bristol.ac.uk>
2023-11-22 13:27:12 +0000

    still need to do menu item controller

---
+++ online-delivery-system/src/main/java/com/sep/onlinedeliverysystem/services/MenuItemService.java
@@ -0,0 +1,23 @@
+package com.sep.onlinedeliverysystem.services;
+
+import com.sep.onlinedeliverysystem.domain.entities.MenuItem;
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public interface MenuItemService {
+    MenuItem save(MenuItem userEntity);
+
+    List<MenuItem> findAll();
+
+    Optional<MenuItem> findOne(Long id);
+
+    boolean Exists(Long id);
+
+    MenuItem partialUpdate(Long id, MenuItem menuItemEntity);
+
+    void delete(Long id);
+}
