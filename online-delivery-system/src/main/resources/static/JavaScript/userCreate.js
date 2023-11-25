1993cc96fbc3be280b639256f53db728b1aaa365
zq22843 <zq22843@bristol.ac.uk>
2023-11-25 18:10:55 +0000

    adds user to the database but doesn't change page

--- online-delivery-system/src/main/resources/static/JavaScript/userCreate.js
+++ online-delivery-system/src/main/resources/static/JavaScript/userCreate.js
@@ -1,10 +1,21 @@
 function createCustomer(){
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value
-    var email = document.getElementById("email").value
-    var checkPassword = document.getElementById("confirm-password").value
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value
+    let email = document.getElementById("email").value
+    let checkPassword = document.getElementById("confirm-password").value
 
-    if(checkPassword.toString() == password.toString()){
+    let valid = checkPassword == password
+
+    if(valid){
+        valid = /\S+@\S+\.\S+/.test(email)
+    }
+
+    if(username.toString() == "" || password.toString() == "" || checkPassword.toString() == ""){
+        valid = false
+    }
+
+
+    if(valid){
         // Send data to the backend
         fetch('/users', {
             method: 'POST',
