40b031d044a6f89f880d325d6fad54a95e5f0575
zq22843 <zq22843@bristol.ac.uk>
2023-11-25 18:10:55 +0000

    adds user to the database but doesn't change page

--- online-delivery-system/src/main/java/com/sep/onlinedeliverysystem/controller/UserController.java
+++ online-delivery-system/src/main/java/com/sep/onlinedeliverysystem/controller/UserController.java
@@ -27,6 +27,7 @@ public class UserController {
 
     @PostMapping(path = "/users")
     public ResponseEntity<UserDTO> save(@RequestBody UserDTO user){ //Create functionality
+        System.out.println("Starting");
         UserEntity userEntity = userMapper.mapFrom(user);
         UserEntity savedUserEntity = userService.save(userEntity); //saves user DTO as entity into our database
         System.out.println("GOT HERE");
