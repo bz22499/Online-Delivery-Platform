31eeeaffcf1881dd6dd52fe456b6efe04b3cdbf6
zq22843 <zq22843@bristol.ac.uk>
2023-11-28 14:18:33 +0000

    split username into first name and last name

--- online-delivery-system/src/main/resources/static/JavaScript/userCreate.js
+++ online-delivery-system/src/main/resources/static/JavaScript/userCreate.js
@@ -1,5 +1,6 @@
 function createCustomer(){
-    let username = document.getElementById("username").value;
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
     let password = document.getElementById("password").value
     let email = document.getElementById("email").value
     let checkPassword = document.getElementById("confirm-password").value
@@ -10,7 +11,7 @@ function createCustomer(){
         valid = /\S+@\S+\.\S+/.test(email)
     }
 
-    if(username.toString() === "" || password.toString() === "" || checkPassword.toString() === ""){
+    if(firstName.toString() === "" || lastName.toString() === "" || password.toString() === "" || checkPassword.toString() === ""){
         valid = false
     }
 
@@ -22,7 +23,7 @@ function createCustomer(){
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ username:username,password:password,email:email})
+            body: JSON.stringify({firstName: firstName, lastName: lastName, password:password,email:email})
         })
             .then(response => response.json())
             .then(data => {
