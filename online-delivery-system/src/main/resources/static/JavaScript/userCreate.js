cdbfb43d57c76f4ce5ed2feb91a61907bd406a67
zq22843 <zq22843@bristol.ac.uk>
2023-11-28 14:07:24 +0000

    added javascript for vendor signup

--- online-delivery-system/src/main/resources/static/JavaScript/userCreate.js
+++ online-delivery-system/src/main/resources/static/JavaScript/userCreate.js
@@ -4,13 +4,13 @@ function createCustomer(){
     let email = document.getElementById("email").value
     let checkPassword = document.getElementById("confirm-password").value
 
-    let valid = checkPassword == password
+    let valid = checkPassword === password
 
     if(valid){
         valid = /\S+@\S+\.\S+/.test(email)
     }
 
-    if(username.toString() == "" || password.toString() == "" || checkPassword.toString() == ""){
+    if(username.toString() === "" || password.toString() === "" || checkPassword.toString() === ""){
         valid = false
     }
 
@@ -35,5 +35,41 @@ function createCustomer(){
     }else{
         alert("Form was not correctly filled in")
     }
+}
 
+function createVendor(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value
+    let email = document.getElementById("email").value
+    let checkPassword = document.getElementById("confirm-password").value
+    let valid = checkPassword === password
+    if(valid){
+        valid = /\S+@\S+\.\S+/.test(email)
+    }
+
+    if(username.toString() === "" || password.toString() === "" || checkPassword.toString() === ""){
+        valid = false
+    }
+
+
+    if(valid){
+        // Send data to the backend
+        fetch('/vendors/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username:username,password:password,email:email})
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Handle the response from the backend
+                window.location.href = '/home';
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }else{
+        alert("Form was not correctly filled in")
+    }
 }
