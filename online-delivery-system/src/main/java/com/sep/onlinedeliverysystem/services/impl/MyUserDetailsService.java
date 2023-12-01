30d48107abb8a905555380efbe1c1b55c5297a2b
hugh k <bz22499@bristol.ac.uk>
2023-12-01 14:38:28 +0000

    started security

---
+++ online-delivery-system/src/main/java/com/sep/onlinedeliverysystem/services/impl/MyUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.sep.onlinedeliverysystem.services.impl;
+
+import java.util.HashSet;
+import java.util.Set;
+import com.sep.onlinedeliverysystem.domain.entities.User;
+import com.sep.onlinedeliverysystem.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findById(email);
+        if(user==null){
+            new UsernameNotFoundException("User not exists by Email");
+        }
+        Set<GrantedAuthority> authorities = new HashSet<>();
+                authorities.add(new SimpleGrantedAuthority("USER"));
+        return new org.springframework.security.core.userdetails.User(email,user.get().getPassword(), authorities);
+    }
+}
+
