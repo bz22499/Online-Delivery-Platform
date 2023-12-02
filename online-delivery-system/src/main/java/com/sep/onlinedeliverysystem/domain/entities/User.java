ab9fa59c39082e239716840f08cd5071eeb40070
hugh k <bz22499@bristol.ac.uk>
2023-12-02 02:31:09 +0000

    values now exist but are never valid

--- online-delivery-system/src/main/java/com/sep/onlinedeliverysystem/domain/entities/User.java
+++ online-delivery-system/src/main/java/com/sep/onlinedeliverysystem/domain/entities/User.java
@@ -11,9 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 @Data
 @AllArgsConstructor
@@ -21,10 +19,50 @@ import java.util.Set;
 @Builder
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails{
     @Id
     private String email;
     private String firstName;
     private String lastName;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Assuming all vendors have the same role for simplicity.
+        return Collections.singletonList(new SimpleGrantedAuthority("VENDOR"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // You can implement your own logic here if needed.
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // You can implement your own logic here if needed.
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // You can implement your own logic here if needed.
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // You can implement your own logic here if needed.
+        return true;
+    }
 }
