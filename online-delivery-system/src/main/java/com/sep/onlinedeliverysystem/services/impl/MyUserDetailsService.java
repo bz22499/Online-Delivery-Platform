5ae126cf555a673d856c2484aa90a250cf0c5c61
hugh k <bz22499@bristol.ac.uk>
2023-12-03 15:25:16 +0000

    success!!!!

--- online-delivery-system/src/main/java/com/sep/onlinedeliverysystem/services/impl/MyUserDetailsService.java
+++ online-delivery-system/src/main/java/com/sep/onlinedeliverysystem/services/impl/MyUserDetailsService.java
@@ -1,6 +1,7 @@
 package com.sep.onlinedeliverysystem.services.impl;
 
 import com.sep.onlinedeliverysystem.domain.entities.User;
+import com.sep.onlinedeliverysystem.domain.entities.UserDetailsEntity;
 import com.sep.onlinedeliverysystem.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -25,14 +26,17 @@ public class MyUserDetailsService implements UserDetailsService {
         User user = userOptional
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
 
+        UserDetailsEntity userDetails = user.toUserDetails();
+
+
         return org.springframework.security.core.userdetails.User
-                .withUsername(user.getUsername())
-                .password(user.getPassword())
+                .withUsername(userDetails.getUsername())
+                .password(userDetails.getPassword())
                 .authorities("USER") // Add roles or authorities as needed
-                .accountExpired(!user.isAccountNonExpired())
-                .accountLocked(!user.isAccountNonLocked())
-                .credentialsExpired(!user.isCredentialsNonExpired())
-                .disabled(!user.isEnabled())
+                .accountExpired(!userDetails.isAccountNonExpired())
+                .accountLocked(!userDetails.isAccountNonLocked())
+                .credentialsExpired(!userDetails.isCredentialsNonExpired())
+                .disabled(!userDetails.isEnabled())
                 .build();
     }
 }
