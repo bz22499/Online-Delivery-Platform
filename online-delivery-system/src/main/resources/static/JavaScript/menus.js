e945fe5a3872527f800d525ec0d7dfcdf3c9f633
zq22843 <zq22843@bristol.ac.uk>
2024-01-25 15:02:02 +0000

    now the home search bar takes the user to the order page where that data can be accessed

--- online-delivery-system/src/main/resources/static/JavaScript/menus.js
+++ online-delivery-system/src/main/resources/static/JavaScript/menus.js
@@ -11,6 +11,19 @@ async function fetchRestaurants(page = 0, size = 18) {
     }
 }
 
+async function fetchAllRestaurants(){
+    try {
+        const response = await fetch(`/vendors`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching restaurant data:', error);
+    }
+}
+
 function populateGrid(pageData) {
     const gridContainer = document.querySelector('.grid-container');
 
@@ -66,7 +79,7 @@ async function loadMore() {
     if (isLoading) return;
     isLoading = true;
 
-    const restaurants = await fetchRestaurants(currentPage, 18);
+    const restaurants = await fetchRestaurants();
     if (restaurants && restaurants.content.length > 0) {
         populateGrid(restaurants);
         currentPage++;
