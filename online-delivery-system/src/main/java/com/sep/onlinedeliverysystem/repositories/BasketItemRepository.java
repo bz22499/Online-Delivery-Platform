64c6d54f80ee9659bbb1949e43da9c26bffaed86
jp-ruiz <vt22223@bristol.ac.uk>
2024-01-25 20:54:07 +0000

    Added entity, repository and tests for orders, baskets, and baske items

---
+++ online-delivery-system/src/main/java/com/sep/onlinedeliverysystem/repositories/BasketItemRepository.java
@@ -0,0 +1,6 @@
+package com.sep.onlinedeliverysystem.repositories;
+
+import com.sep.onlinedeliverysystem.domain.entities.BasketItem;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BasketItemRepository extends CrudRepository<BasketItem, Long> {}
