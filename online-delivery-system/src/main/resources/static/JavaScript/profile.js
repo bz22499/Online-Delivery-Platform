6498cdd5c18b3e92a54cff716ae83656ac7022dc
ek21604 <ek21604@bristol.ac.uk>
2024-02-10 16:07:17 +0000

    Bugs fixed, however in the process I removed the functionality where the user has to sign in first

--- online-delivery-system/src/main/resources/static/JavaScript/profile.js
+++ online-delivery-system/src/main/resources/static/JavaScript/profile.js
@@ -13,15 +13,8 @@ function updateProfilePicture() {
     }
 }
 function editProfile() {
-    /*document.getElementById('new-firstName').style.display = 'block';
-    document.getElementById('new-lastName').style.display = 'block';
-    document.getElementById('new-job').style.display = 'block';
-    document.getElementById('new-description').style.display = 'block';
-    document.getElementById('view-subscription').style.display = 'none';
-    document.getElementById('logout').style.display = 'none';*/
     document.getElementById('user-details').style.display = 'none';
-
-    document.getElementById('edit-form').style.display = 'inline-block';
+    document.getElementById('edit-form').style.display = 'block';
 
     // Set the current values in the form fields
     document.getElementById('new-firstName').value = document.getElementById('firstName').innerText;
@@ -29,21 +22,12 @@ function editProfile() {
     document.getElementById('new-job').value = document.getElementById('job').innerText;
     document.getElementById('new-description').value = document.getElementById('description').innerText;
 
-
     // Hide buttons in the user-details div
     document.querySelector('#user-details button').style.display = 'none';
 }
 
 function returnToProfile() {
-    /*document.getElementById('new-firstName').style.display = 'block';
-    document.getElementById('new-lastName').style.display = 'block';
-    document.getElementById('new-job').style.display = 'block';
-    document.getElementById('new-description').style.display = 'block';
-    document.getElementById('view-subscription').style.display = 'inline-block';
-    document.getElementById('logout').style.display = 'inline-block';
-    //document.getElementById('edit-profile').style.display = 'block';*/
     document.getElementById('user-details').style.display = 'block';
-
     document.getElementById('edit-form').style.display = 'none';
 }
 
@@ -52,36 +36,16 @@ function cancelEdit() {
 }
 
 function saveProfile() {
-    var newFirstName = document.getElementById('new-firstName').value;
-    var newLastName = document.getElementById('new-lastName').value;
-    var newJob = document.getElementById('new-job').value;
-    var newDescription = document.getElementById('new-description').value;
-    var profileData = {
-        firstName: newFirstName,
-        lastName: newLastName,
-        job: newJob,
-        description: newDescription,
-        profilePicture: document.getElementById('previewImage').src
-    };
+    // Update profile information with the edited values
+    document.getElementById('firstName').innerText = document.getElementById('new-firstName').value;
+    document.getElementById('lastName').innerText = document.getElementById('new-lastName').value;
+    document.getElementById('job').innerText = document.getElementById('new-job').value;
+    document.getElementById('description').innerText = document.getElementById('new-description').value;
 
-    // Convert the profileData object to a JSON string
-    var profileDataString = JSON.stringify(profileData);
-
-    // Store the JSON string in the local storage
-    localStorage.setItem('userProfile', profileDataString);
-
+    // Show profile section and hide edit form
     document.getElementById('user-details').style.display = 'block';
-
     document.getElementById('edit-form').style.display = 'none';
-    
+
     // Inform the user that the profile has been saved
     alert("Profile saved!");
-
-    //update the displayed details
-    /*document.getElementById('firstName').innerText = newFirstName;
-    document.getElementById('lastName').innerText = newLastName;
-    document.getElementById('job').innerText = newJob;
-    document.getElementById('description').innerText = newDescription;*/
-
-    //returnToProfile();
 }
