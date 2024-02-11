583c33103038c432e8313cb6d99ee8171cc1e0c7
hugh k <bz22499@bristol.ac.uk>
2024-02-11 16:43:30 +0000

    vendor pages linked together

--- online-delivery-system/src/main/java/com/sep/onlinedeliverysystem/controller/GlobalControllerAdvice.java
+++ online-delivery-system/src/main/java/com/sep/onlinedeliverysystem/controller/GlobalControllerAdvice.java
@@ -1,21 +1,49 @@
 package com.sep.onlinedeliverysystem.controller;
 
+import com.sep.onlinedeliverysystem.domain.entities.User;
+import com.sep.onlinedeliverysystem.domain.entities.Vendor;
+import com.sep.onlinedeliverysystem.services.UserService;
+import com.sep.onlinedeliverysystem.services.VendorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.security.Principal;
+import java.util.Optional;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
-    //All controllers can see the value of loggedIn
+    private final UserService userService;
+    private final VendorService vendorService;
+
+    @Autowired
+    public GlobalControllerAdvice(UserService userService, VendorService vendorService) {
+        this.userService = userService;
+        this.vendorService = vendorService;
+    }
+
+    // All controllers can see the value of loggedIn, loggedInUser, and loggedInVendor
     @ModelAttribute
     public void globalAttributes(Principal principal, Model model) {
-        if (principal != null) {
-            model.addAttribute("loggedIn", true); // User is logged in
-        } else {
-            model.addAttribute("loggedIn", false); // User is not logged in
+        boolean loggedIn = (principal != null);
+        model.addAttribute("loggedIn", loggedIn); // User is logged in or not
+
+        boolean loggedInUser = false;
+        boolean loggedInVendor = false;
+
+        if (loggedIn) {
+            String loggedInUserEmail = principal.getName();
+            Optional<User> user = userService.findOne(loggedInUserEmail);
+            Optional<Vendor> vendor = vendorService.findOne(loggedInUserEmail);
+
+            loggedInUser = user.isPresent();
+            loggedInVendor = vendor.isPresent();
         }
+
+        model.addAttribute("loggedInUser", loggedInUser); // logged in and is a user
+        model.addAttribute("loggedInVendor", loggedInVendor); // logged in and is a vendor
     }
 }
