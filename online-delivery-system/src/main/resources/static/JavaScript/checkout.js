95ada01821061b26d67a44c56b3e585c0f08fb3e
ek21604 <ek21604@bristol.ac.uk>
2024-03-07 00:22:42 +0000

    cleaned up files and added first iteration of styling

--- online-delivery-system/src/main/resources/static/JavaScript/checkout.js
+++ online-delivery-system/src/main/resources/static/JavaScript/checkout.js
@@ -1,39 +1,3 @@
-/*document.addEventListener('DOMContentLoaded', function () {
-    const basketSummary = document.querySelector('.basket-summary');
-    const totalCost = document.querySelector('.total-cost');
-    const payButton = document.querySelector('.pay-button');
-
-    // Function to calculate and display the basket summary and total cost
-    function displayBasketSummary() {
-        basketSummary.innerHTML = ''; // Clear previous content
-
-        // Iterate through items in the basket
-        for (const item of basket.items) {
-            const basketItemSummary = document.createElement('div');
-            basketItemSummary.textContent = `${item.menuItem.name} x${item.quantity}`;
-            basketSummary.appendChild(basketItemSummary);
-        }
-
-        // Calculate total cost
-        const totalPrice = basket.items.reduce((total, item) => {
-            return total + item.menuItem.price * item.quantity;
-        }, 0);
-
-        totalCost.textContent = `Total: £${totalPrice.toFixed(2)}`;
-    }
-
-    // Display basket summary on page load
-    displayBasketSummary();
-
-    // Event listener for the "Pay Now" button
-    payButton.addEventListener('click', function () {
-        alert('Payment successful! Redirecting to order confirmation page.');
-
-        // Redirect to order confirmation page
-        window.location.href = '/order-confirmation';
-    });
-});*/
-
 let isFetchingBaskets = false;
 
 // get all the baskets for current order ID
