78ea4ba27da1ab64766baa96140670a90832adc8
zq22843 <zq22843@bristol.ac.uk>
2024-03-11 13:05:48 +0000

    it now works when it stores on local

---
+++ online-delivery-system/src/main/java/com/sep/onlinedeliverysystem/config/StaticConfig.java
@@ -0,0 +1,14 @@
+package com.sep.onlinedeliverysystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:src/main/resources/uploads/");
+    }
+}
