efc8ecb7ffc8fbc31f7fa20263edb94ad7f6254c
Hugh <bz22499@bristol.ac.uk>
2024-04-04 21:42:55 +0100

    end of day 4/4

--- online-delivery-system/src/main/resources/static/JavaScript/ordersForVendors.js
+++ online-delivery-system/src/main/resources/static/JavaScript/ordersForVendors.js
@@ -1,9 +1,8 @@
 let isLoading = false;
 let currentPage = 0;
-let loggedInVendorEmail = ''; // Define a variable to store the logged-in vendor's email
+let loggedInVendorId = ''; // Define a variable to store the logged-in vendor's id
 
-// Fetch orders associated with the currently logged-in vendor
-async function fetchOrders(page= 0, size = 10) {
+async function fetchOrders(page = 0, size = 10) {
     try {
         const response = await fetch(`/orders?page=${page}&size=${size}`);
         if (!response.ok) {
@@ -16,7 +15,6 @@ async function fetchOrders(page= 0, size = 10) {
     }
 }
 
-// Fetch baskets associated with an order
 async function fetchBasketsByOrder(orderId) {
     try {
         const response = await fetch(`/baskets/orders/${orderId}`);
@@ -24,17 +22,6 @@ async function fetchBasketsByOrder(orderId) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const baskets = await response.json();
-
-        // Fetch basket items for each basket
-        for (const basket of baskets) {
-            const basketItemsResponse = await fetch(`/basketItems/baskets/${basket.id}`);
-            if (!basketItemsResponse.ok) {
-                throw new Error(`HTTP error! status: ${basketItemsResponse.status}`);
-            }
-            const basketItems = await basketItemsResponse.json();
-            basket.basketItems = basketItems; // Assign fetched basketItems to basket object
-        }
-
         return baskets;
     } catch (error) {
         console.error('Error fetching baskets:', error);
@@ -42,22 +29,12 @@ async function fetchBasketsByOrder(orderId) {
     }
 }
 
-// Populate orders associated with the currently logged-in vendor
 async function populateOrders(ordersData) {
     const gridContainer = document.querySelector('.grid-container');
 
     if (ordersData && ordersData.content) {
         for (const order of ordersData.content) {
-            // Check if the order contains menu items associated with the logged-in vendor
-            const hasMenuItemsForVendor = order.baskets.some(basket =>
-                basket.basketItems.some(item =>
-                    item.menuItem.vendor.email === loggedInVendorEmail
-                )
-            );
-
-            if (!hasMenuItemsForVendor) {
-                continue; // Skip orders that don't have menu items for the logged-in vendor
-            }
+            let orderContainsVendorItems = false;
 
             const orderItem = document.createElement('div');
             orderItem.className = 'order-item';
@@ -66,40 +43,40 @@ async function populateOrders(ordersData) {
             orderInfo.textContent = `Order ID: ${order.id}`;
             orderItem.appendChild(orderInfo);
 
-            const basketList = document.createElement('ul');
-            basketList.className = 'basket-list';
-
-            for (const basket of order.baskets) {
-                for (const basketItem of basket.basketItems) {
-                    const menuItem = basketItem.menuItem;
-                    if (menuItem.vendor.email !== loggedInVendorEmail) {
-                        continue; // Skip menu items not associated with the logged-in vendor
+            // Fetch baskets associated with the order
+            const baskets = await fetchBasketsByOrder(order.id);
+            if (baskets && baskets.length > 0) {
+                const basketList = document.createElement('ul');
+                basketList.className = 'basket-list';
+                for (const basket of baskets) {
+                    const basketItems = basket.basketItems;
+                    for (const basketItem of basketItems) {
+                        const menuItem = basketItem.menuItem;
+                        if (menuItem.vendor.id === loggedInVendorId) {
+                            orderContainsVendorItems = true;
+                            const basketItemInfo = document.createElement('li');
+                            const totalPrice = menuItem ? (menuItem.price * basketItem.quantity).toFixed(2) : 'N/A';
+                            basketItemInfo.textContent = `Basket ID: ${basket.id}, Basket Item ID: ${basketItem.id}, Menu Item Name: ${menuItem.name}, Quantity: ${basketItem.quantity}, Price per Item: ${menuItem.price.toFixed(2)}, Total Price: ${totalPrice}`;
+                            basketList.appendChild(basketItemInfo);
+                        }
                     }
-                    const basketItemInfo = document.createElement('li');
-                    const totalPrice = menuItem ? (menuItem.price * basketItem.quantity).toFixed(2) : 'N/A';
-                    basketItemInfo.textContent = `Basket ID: ${basket.id}, Basket Item ID: ${basketItem.id}, Menu Item Name: ${menuItem.name}, Quantity: ${basketItem.quantity}, Price per Item: ${menuItem.price.toFixed(2)}, Total Price: ${totalPrice}`;
-                    basketList.appendChild(basketItemInfo);
+                }
+                if (orderContainsVendorItems) {
+                    orderItem.appendChild(basketList);
+                    gridContainer.appendChild(orderItem);
                 }
             }
-
-            if (basketList.childNodes.length > 0) {
-                orderItem.appendChild(basketList);
-            } else {
-                const noBasketInfo = document.createElement('div');
-                noBasketInfo.textContent = 'No basket items found for this order associated with the logged-in vendor.';
-                orderItem.appendChild(noBasketInfo);
-            }
-
-            gridContainer.appendChild(orderItem);
+            console.log('Order ID:', order.id, 'Contains Vendor Items:', orderContainsVendorItems);
         }
     }
 }
+
 
 // Function to load more orders
 async function loadMore() {
     if (isLoading) return;
     isLoading = true;
-    const orders = await fetchOrders(currentPage, 10);
+    const orders = await fetchOrders(currentPage, 30);
     if (orders && orders.content.length > 0) {
         await populateOrders(orders);
         currentPage++;
@@ -109,8 +86,8 @@ async function loadMore() {
 
 // Load initial set of orders when the window loads
 window.onload = async () => {
-    // Fetch the currently logged-in vendor's email from the hidden div
-    loggedInVendorEmail = document.getElementById('loggedInVendorEmail').getAttribute('data-email');
+    // Fetch the currently logged-in vendor's id from the hidden div
+    loggedInVendorId = document.getElementById('loggedInVendorEmail').getAttribute('data-email');
     await loadMore();
 };
 
@@ -119,13 +96,4 @@ window.addEventListener('scroll', () => {
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 && !isLoading) {
         loadMore();
     }
-});
-
-// Toggle visibility of baskets dropdown on button click
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('viewBasketsButton').addEventListener('click', async function(event) {
-        event.preventDefault();
-        const dropdown = document.getElementById('basketsDropdown');
-        dropdown.style.display = dropdown.style.display === 'none' ? 'block' : 'none';
-    });
 });
