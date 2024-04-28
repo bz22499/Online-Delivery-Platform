13758b64c7db38fff209be96dfb385825bff8865
ek21604 <ek21604@bristol.ac.uk>
2024-04-28 12:30:50 +0100

    tests written and pass for UserAddressService

--- online-delivery-system/src/test/java/com/sep/onlinedeliverysystem/services/UserAddressServiceIntegrationTests.java
+++ online-delivery-system/src/test/java/com/sep/onlinedeliverysystem/services/UserAddressServiceIntegrationTests.java
@@ -1,13 +1,18 @@
 package com.sep.onlinedeliverysystem.services;
 
-package com.sep.onlinedeliverysystem.services;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.sep.onlinedeliverysystem.domain.entities.*;
 import com.sep.onlinedeliverysystem.repositories.MenuItemRepository;
 import com.sep.onlinedeliverysystem.repositories.OrderRepository;
+import com.sep.onlinedeliverysystem.repositories.UserAddressRepository;
+
+import com.sep.onlinedeliverysystem.repositories.UserRepository;
+import com.sep.onlinedeliverysystem.services.UserService;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +21,6 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.sep.onlinedeliverysystem.TestUtil;
-import com.sep.onlinedeliverysystem.domain.entities.Basket;
-import com.sep.onlinedeliverysystem.domain.entities.BasketItem;
-import com.sep.onlinedeliverysystem.domain.entities.MenuItem;
-import com.sep.onlinedeliverysystem.domain.entities.Order;
-import com.sep.onlinedeliverysystem.domain.entities.Vendor;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -32,18 +32,43 @@ public class UserAddressServiceIntegrationTests {
     private OrderRepository orderRepository;
     private MenuItemRepository menuItemRepository;
     private VendorService vendorService;
+    private UserService userService;
+    private UserAddressRepository userAddressRepository;
+    private UserRepository userRepository;
 
     @Autowired
-    public UserAddressServiceIntegrationTests(BasketService basketService, BasketItemService basketItemService, OrderRepository orderRepository, MenuItemRepository menuItemRepository, VendorService vendorService) {
+    public UserAddressServiceIntegrationTests(BasketService basketService, BasketItemService basketItemService, OrderRepository orderRepository, MenuItemRepository menuItemRepository, VendorService vendorService, UserService userService, UserAddressRepository userAddressRepository, UserRepository userRepository) {
         this.basketService = basketService;
         this.basketItemService = basketItemService;
         this.orderRepository = orderRepository;
         this.menuItemRepository = menuItemRepository;
         this.vendorService = vendorService;
+        this.userService = userService;
+        this.userAddressRepository = userAddressRepository;
+        this.userRepository = userRepository;
     }
 
     @Test
     public void testFindByUserEmail(){
 
+        User user1 = TestUtil.userBuild1();
+        User user2 = TestUtil.userBuild1();
+        User user3 = TestUtil.userBuild1();
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.save(user3);
+
+        UserAddress userAddress1 = TestUtil.userAddressBuild1(user1);
+        UserAddress userAddress2 = TestUtil.userAddressBuild1(user2);
+        UserAddress userAddress3 = TestUtil.userAddressBuild1(user3);
+
+        userAddressRepository.save(userAddress1);
+        userAddressRepository.save(userAddress2);
+        userAddressRepository.save(userAddress3);
+
+        Optional<User> userAddressFound = userService.findOne(String.valueOf(userAddress1));
+
+        assertThat(userAddressFound).isNotNull();
     }
 }
